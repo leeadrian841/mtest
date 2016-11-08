3dfdba994d49259984330e1e3cbb2b52fb636bc5
Henson Tay <hensontay@gmail.com>
2016-11-08 15:33:24 +0800

    added downloads column to songs table

--- db/migrate/20161108070242_song.rb
+++ db/migrate/20161108070242_song.rb
@@ -1,10 +1,11 @@
 class Song < ActiveRecord::Migration[5.0]
   def change
-      create_table :songs do |t|
-        t.string :title
-        t.string :duration
-        t.string :year_release
-        t.string :album_title
+        create_table :songs do |t|
+          t.string :title
+          t.string :duration
+          t.string :year_release
+          t.string :album_title
+          t.string :downloads
+      end
   end
-end
 end
